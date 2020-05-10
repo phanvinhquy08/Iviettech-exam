98dd463bc378a944eb6b7ba3a819ae335ac321ce
phanvinhquy08 <58554582+phanvinhquy08@users.noreply.github.com>
2020-05-10 22:40:19 +0700

    finish

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+
+import Header from './Components/Header';
+
+
+const data = [
+  { id: 0, code: 451, name: "Blizzard", color: "#FFFFCC" },
+  { id: 1, code: 468, name: "Hawk", color: "#FFFF99" },
+  { id: 2, code: 485, name: "Canopy", color: "#FFFF66" },
+  { id: 3, code: 452, name: "Bone", color: "#CCFF66" },
+  { id: 4, code: 469, name: "Red fox", color: "#66FF99" },
+  { id: 5, code: 486, name: "Verde", color: "#FFCC99" },
+  { id: 6, code: 453, name: "Teepee", color: "#CCCC99" },
+  { id: 7, code: 470, name: "Big Sky", color: "#99CC99" },
+  { id: 8, code: 487, name: "Helena", color: "#99CC66" },
+  { id: 9, code: 454, name: "Sandstone", color: "#FF99CC" },
+  { id: 10, code: 471, name: "Glacier", color: "#FF9999" },
+  { id: 11, code: 488, name: "Ponderosa Pine", color: "#00CC66" },
+  { id: 12, code: 455, name: "Fawn", color: "#FF9900" },
+  { id: 13, code: 472, name: "Great Fall", color: "#9999FF" },
+  { id: 14, code: 489, name: "Lewis & Clark", color: "#9966CC" },
+];
+
+class ShowColor extends Component {
+  render() {
+    const style = {
+      width: "300px",
+      display: "flex",
+      height: "50px",
+      margin: "10px"
+    };
+    const { code, name, color } = this.props;
+    return (
+      <div style={style}>
+        <div style={{textAlign: "right", width:"150px"}}>
+          <div>{code}</div>
+          <div>{name}</div>
+        </div>
+        <div style={{ width: "250px", background: color, marginLeft: "5px" }} />
+      </div>
+    )
+  }
+}
+
+class List extends Component {
+  render() {
+    const { data } = this.props;
+    const style = {
+      display: "flex",
+      flexWrap: "wrap"
+    }
+    return (
+      <div style={style}>
+        {data.map(item => <ShowColor key={item.id} {...item} />)}
+      </div>
+    )
+  }
+}
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <List data={data} />
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div>
+                <h1>So Fine</h1>
+                <h3>By join Flynn</h3>
+                <h3>Great for Quilting & Sewing</h3>
+            </div>
+        )
+    }
+}
