5f7723e2b28f1b0bd12682968872aae9d9325eaf
phanvinhquy08 <58554582+phanvinhquy08@users.noreply.github.com>
2020-05-13 19:51:47 +0700

    bai 1

--- Bài 1/src/Components/ShowColor.js
+++ Bài 1/src/Components/ShowColor.js
@@ -4,11 +4,11 @@ export default class ShowColor extends Component {
     render() {
         const style = {
             float: "left",
-            width: "300px"
+            
         };
         return (
             <div style={style}>
-                A
+                {}
             </div>
         )
     }
